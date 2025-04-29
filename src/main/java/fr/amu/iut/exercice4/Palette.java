d4322ff3fc208c48cb239009b3699fbf022aab20
Samuel <samuel.celestine@etu.univ-amu.fr>
2025-04-29 16:36:41 +0200

    commit

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,12 +1,19 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.security.cert.PolicyNode;
 
 public class Palette extends Application {
 
@@ -25,6 +32,48 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        root = new BorderPane();
+
+        // Création d'un conteneur VBox avec ses éléments centrés
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
+        root.setBottom(hbox);
+
+        // Création de la scene
+        Scene scene = new Scene(root);
+
+        // Ajout de la scene à la fenêtre
+        primaryStage.setScene(scene);
+
+        panneau = new Pane();
+        root.setCenter(panneau);
+
+        // Ajout d'un bouton avec du texte
+        Button button = new Button("Vert");
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            panneau.setStyle("-fx-background-color: #56c90d; ");
+        });
+        hbox.getChildren().add( button );
+        hbox.setAlignment(Pos.BOTTOM_CENTER);
+
+        // Ajout d'un bouton avec du texte
+        Button button1 = new Button("Rouge");
+        hbox.getChildren().add( button1 );
+        button1.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            panneau.setStyle("-fx-background-color: #ff0000; ");
+        });
+
+        // Ajout d'un bouton avec du texte
+        Button button2 = new Button("Bleu");
+        hbox.getChildren().add( button2 );
+        button2.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            panneau.setStyle("-fx-background-color: #0c8ebd; ");
+        });
+
+        primaryStage.setTitle("");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
         primaryStage.show();
     }
 }
