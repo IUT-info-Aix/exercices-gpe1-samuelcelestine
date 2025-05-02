b42113601ac4cbdbc770ebd9803b286214bff733
Samuel <samuel.celestine@etu.univ-amu.fr>
2025-05-02 16:34:01 +0200

    commit

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,28 @@
 package fr.amu.iut.exercice8;
 
+import com.example.partie3.AlertHelper;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Window;
 
 public class LoginControl extends GridPane {
 
-    private void okClicked() {
-    }
 
+    @FXML
+    private TextField click;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private void okClicked() {
+
+        }
+
+    @FXML
     private void cancelClicked() {
-    }
+
 }
--- src/main/java/fr/amu/iut/exercice8/LoginMain.java
+++ src/main/java/fr/amu/iut/exercice8/LoginMain.java
@@ -1,8 +1,11 @@
 package fr.amu.iut.exercice8;
 
+import fr.amu.iut.exercice4.BonjourFenetre;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +21,7 @@ public class LoginMain extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("exercice8/LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+        //scene.getStylesheets().addAll(this.getClass().getResource("Login.css").toExternalForm());
         stage.setTitle("FXML Custom Control");
         stage.show();
     }
