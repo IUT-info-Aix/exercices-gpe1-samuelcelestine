cb7adb9524c159e4a562199d5ad018ad1ada36cc
Samuel <samuel.celestine@etu.univ-amu.fr>
2025-05-02 15:28:38 +0200

    commit

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,22 +1,42 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
+    @FXML
+    Label counterLabel;
+
+    @FXML
+    Button decrementButton;
+
+    @FXML
+    Button incrementButton;
+
     int counter = 0;
 
+    @FXML
     public void increment() {
+        counter++;
+        counterLabel.setText(String.valueOf(counter));
     }
 
+    @FXML
     public void decrement() {
+        counter--;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing CounterController...");
-   }
+    }
 }
